d78247c7d9ba3c67c34a3b3b452111678b1c6771
davidwells <david@inboundnow.com>
2018-04-26 10:30:02 -0700

    fix test

--- src/utils/fs/getTmpDir.js
+++ src/utils/fs/getTmpDir.js
@@ -1,11 +1,22 @@
 const os = require('os')
+const fs = require('fs')
 const path = require('path')
 const crypto = require('crypto')
 const fse = require('../fs/fse')
 
+const TMP_DIR = os.tmpdir()
+// Workaround for https://github.com/nodejs/node/issues/11422
+let resolved
+const getActualTmpDir = () => {
+  if (!resolved) {
+    resolved = fs.realpathSync(TMP_DIR)
+  }
+  return resolved
+}
+
 async function getTmpDir() {
   const tmpDirPath = path.join(
-    os.tmpdir(),
+    getActualTmpDir(),
     'tmpdirs-serverless-components',
     crypto.randomBytes(8).toString('hex')
   )
